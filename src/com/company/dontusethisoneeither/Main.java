50ea14020e2548ea031f153c127ce9dc22d84749
Eli Terry <e.terry1135@gmail.com>
2022-09-26 10:20:59 -0400

    Initial commit

---
+++ src/com/company/dontusethisoneeither/Main.java
@@ -0,0 +1,92 @@
+package com.company.dontusethisoneeither;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] Args) throws FileNotFoundException {
+
+        int i = 0;
+        int max = 0;
+        ArrayList<String[]> graph = new ArrayList<>();
+        for(int j = 0; j <= 9999; j++) {
+            int startNumber = j;
+            char[] digits = addPreceedingZeros(j);
+
+            char[] lowtohigh = digits, hightolow = digits;
+            Arrays.sort(lowtohigh);
+            Arrays.sort(hightolow);
+            hightolow = reverse(hightolow);
+
+            int output = numberFromArray(hightolow) - numberFromArray(lowtohigh);
+            System.out.println(j);
+            if(output != 0 && output != 6174) {
+                output = j;
+                for(i = 1; output != 6174 && output != 0; i++) {
+                    digits = addPreceedingZeros(output);
+                    lowtohigh = digits;
+                    hightolow = digits;
+                    Arrays.sort(lowtohigh);
+                    Arrays.sort(hightolow);
+                    hightolow = reverse(hightolow);
+                    int highlownum = numberFromArray(hightolow), lowhighnum = numberFromArray(lowtohigh);
+                    output =  highlownum - lowhighnum;
+                    System.out.println(i + ": " + highlownum + " - " + lowhighnum + " = " + output);
+                    if(i > max) max = i;
+                }
+                graph.add(new String[]{( Integer.valueOf(j).toString()), Integer.valueOf(i-1).toString()});
+            } else {
+                System.out.println("1: " + j);
+                graph.add(new String[]{Integer.valueOf(j).toString(), Integer.valueOf(1).toString()});
+            }
+
+        }
+        System.out.println("Max Number: "+ max);
+        System.out.println(graph.toString());
+
+        File csvOutputFile = new File("output.csv");
+        try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
+            graph.stream()
+                    .map(Main::convertToCSV)
+                    .forEach(pw::println);
+        }
+    }
+
+    public static char[] reverse(char[] input) {
+        char[] output = new char[input.length];
+       for(int i = 0; i < input.length; i++) {
+           output[i] = input[input.length - i - 1];
+        }
+       return output;
+    }
+
+    public static int numberFromArray(char[] array) {
+        int output = 0;
+        for(int i = 0; i < array.length; i++) {
+            output += Integer.parseInt(String.valueOf(array[array.length - i - 1])) * Math.pow(10, i);
+        }
+        return output;
+    }
+
+    public static String convertToCSV(String[] data) {
+        return String.join(",", data);
+    }
+
+    public static char[] addPreceedingZeros(int input) {
+        char[] output;
+        if(input>1000) {
+            output = Integer.toString(input).toCharArray();
+        } else if(input>100) {
+            output = ("0" + input).toCharArray();
+        } else if(input>10) {
+            output = ("00" + input).toCharArray();
+        } else output = ("000" + input).toCharArray();
+        return output;
+    }
+}
---
+++ src/com/company/dontusethissoftware/Main.java
@@ -0,0 +1,94 @@
+package com.company.dontusethissoftware;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        final int imageWidth = 1920, imageHeight = 1441, colorTolerance = 90;
+
+        final Color lineColor = new Color(181, 230, 29);
+
+        Color backgroundColor = new Color(255, 255, 255, 255), highlightColor = new Color(0, 0, 0, 255);
+
+        final boolean funMode = true;
+
+
+        BufferedImage image = null;
+        try {
+            File input_file = new File(
+                    "C:/Users/redst/Downloads/COWlove.jpg");
+
+            // image file path create an object of
+            // BufferedImage type and pass as parameter the
+            // width,  height and image int
+            // type. TYPE_INT_ARGB means that we are
+            // representing the Alpha , Red, Green and Blue
+            // component of the image pixel using 8 bit
+            // integer value.
+
+            image = new BufferedImage(
+                    imageWidth, imageHeight, BufferedImage.TYPE_INT_ARGB);
+
+            // Reading input file
+            image = ImageIO.read(input_file);
+
+            System.out.println("Reading complete.");
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+        if(!funMode) {
+            for(int i = 0; i < imageWidth; i++) {
+                for(int j = 0; j < imageHeight; j++) {
+                    Color color = new Color(image.getRGB(i, j), true);
+                    if(ColourDistance(color, lineColor) > colorTolerance ) {
+                        image.setRGB(i, j, backgroundColor.hashCode());
+                    } else image.setRGB(i, j, highlightColor.hashCode());
+                }
+            }
+        } else {
+            for (int i = 0; i < imageWidth; i++) {
+                for (int j = 0; j < imageHeight; j++) {
+                    Color color = new Color(image.getRGB(i, j), true);
+                    if (ColourDistance(color, lineColor) > colorTolerance) {
+                        image.setRGB(i, j, backgroundColor.hashCode());
+                    } else image.setRGB(i, j, new Color(Color.HSBtoRGB((float) i/imageWidth, 1, (float) j/imageHeight)).hashCode());
+                }
+            }
+        }
+
+
+        try {
+            // Output file path
+            File output_file = new File(
+                    "C:/Users/redst/Downloads/COWlovers.png");
+
+            // Writing to file taking type and path as
+            ImageIO.write(image, "png", output_file);
+
+            System.out.println("Writing complete.");
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+
+    }
+
+    public static double ColourDistance(Color c1, Color c2)
+    {
+        double rmean = ( c1.getRed() + c2.getRed() )/2.0;
+        int r = c1.getRed() - c2.getRed();
+        int g = c1.getGreen() - c2.getGreen();
+        int b = c1.getBlue() - c2.getBlue();
+        double weightR = 2 + rmean/256;
+        double weightG = 4.0;
+        double weightB = 2 + (255-rmean)/256;
+        return Math.sqrt(weightR*r*r + weightG*g*g + weightB*b*b);
+    }
+}
